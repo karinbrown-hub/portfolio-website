bf8781ca226d479204ff33c62e5b24d9d9cc7e3d
karinbrown-hub <alexanja464@gmail.com>
2025-02-22 17:13:49 +0300

    Update App.tsx....includes hashing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useHashLocation } from "wouter"; // Added useHashLocation
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -15,7 +15,7 @@ function Router() {
     <div className="min-h-screen bg-background">
       <Navigation />
       <main>
-        <Switch>
+        <Switch hook={useHashLocation}> {/* Added hook={useHashLocation} */}
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/portfolio" component={Portfolio} />
